0ca918e8515529e18522bac1c84b52a836cab7c9
Caio Escorel <69322958+Caioesc@users.noreply.github.com>
2024-11-22 11:36:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const darkModeButton = document.getElementById('dark-mode');
 darkModeButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
+
+const menuButton = document.getElementById("menu-toggle");
+const slideMenu = document.getElementById("slide-menu");
+
+menuButton.addEventListener("click", () => {
+    slideMenu.classList.toggle("open");
+});
