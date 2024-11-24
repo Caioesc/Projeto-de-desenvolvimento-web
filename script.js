aba04d567f6cedfee45f727fb1fc4840cd6ce808
Caio Escorel <69322958+Caioesc@users.noreply.github.com>
2024-11-23 18:49:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const darkModeButton = document.getElementById('dark-mode');
-darkModeButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
-
-const menuButton = document.getElementById("menu-toggle");
+const botaoMenu = document.getElementById("menu-toggle");
 const slideMenu = document.getElementById("slide-menu");
 
-menuButton.addEventListener("click", () => {
+botaoMenu.addEventListener("click", () => {
     slideMenu.classList.toggle("open");
+});
+
+
+function botaoDarkMode() {
+    const body = document.body;
+    const darkModeOn = body.classList.toggle("dark-mode");
+    
+   
+    localStorage.setItem("darkMode", darkModeOn ? "enabled" : "disabled");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const modoSalvo = localStorage.getItem("darkMode");
+    
+    if (modoSalvo === "enabled") {
+        document.body.classList.add("dark-mode");
+    }
 });
